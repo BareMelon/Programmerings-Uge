e84d0e87d59e4996e367ac0e12a51d8db0c2a0d1
Gustav Fyhn <fyhngustav3@gmail.com>
2026-03-23 09:54:17 +0100

    v1.1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SayHelloWorld();
+    }
+
+    private static void SayHelloWorld()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
